39cb4ed99954f9fb6608fde87e05201927c7cc9e
Nelson R. Graña <nelsongraa8@gmail.com>
2025-10-04 19:36:02 +0200

    chore(libs): cambiar de posicion la libreria

--- apps/demo/src/app/app.ts
+++ apps/demo/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { PostMessageModule } from '../../../../libs/ngxs/postmessage-plugin/src/lib/postmessage.module';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, PostMessageModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
