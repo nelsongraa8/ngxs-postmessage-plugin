0021f736aa13275aeb4d0d3eacb3458c2335e94a
Nelson R. Graña <nelsongraa8@gmail.com>
2025-10-11 13:04:15 +0200

    feat(app): import the plugin with the providers

--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -5,11 +5,18 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { providerPostMessagePlugin } from '@ngxs-postmessage/postmessage-plugin';
+import { provideStore } from '@ngxs/store';
+import { CounterState } from './ngxs/counter.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    providerPostMessagePlugin(),
+
+    provideStore(),
+    provideStore([CounterState]),
   ],
 };
--- apps/demo/src/app/app.ts
+++ apps/demo/src/app/app.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
 
+import { PostMessageModule } from '@ngxs-postmessage/postmessage-plugin';
+
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, PostMessageModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  standalone: true,
 })
 export class App {
   protected title = 'demo';
