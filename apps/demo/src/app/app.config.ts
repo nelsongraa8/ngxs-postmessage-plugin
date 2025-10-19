1d9ac9d7425f1f1c291dc0cffa1c45beb03d74e2
Nelson R. Graña <nelsongraa8@gmail.com>
2025-10-19 12:03:22 +0200

    feat(app): send correctly options for configuration of state

--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -9,6 +9,32 @@ import { provideStore } from '@ngxs/store';
 import { CounterState } from '../context/ngxs/counter.state';
 import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
 import { withNgxsPostMessagePlugin } from '@ngxs-postmessage/postmessage-plugin';
+import { NgxsPostmessagePluginOptions } from 'libs/postmessage-plugin/src/lib/providers/postmessage.options';
+
+export const SimplePostMessageConfig: NgxsPostmessagePluginOptions = {
+  send: {
+    actions: [
+      { type: 'Increment', to: ['*'] },
+      { type: 'Decrement', to: ['https://analytics.app'] },
+    ],
+    states: [{ type: 'counter', to: ['*', 'https://analytics.app'] }],
+  },
+};
+
+export const ExtremePostMessageConfig: NgxsPostmessagePluginOptions = {
+  targets: [
+    { name: 'all', origin: ['*'], transfer: ['*'] },
+    {
+      name: 'analytics',
+      origin: ['https://analytics.app'],
+      transfer: [''],
+    },
+  ],
+  send: {
+    actions: [{ type: '*', targets: ['all'], mode: 'all' }],
+    states: [{ type: '*', targets: ['all', 'analytics'], mode: 'change' }],
+  },
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,7 +48,7 @@ export const appConfig: ApplicationConfig = {
         collapsed: false,
         logger: console,
       }),
-      withNgxsPostMessagePlugin()
+      withNgxsPostMessagePlugin(SimplePostMessageConfig)
     ),
   ],
 };
