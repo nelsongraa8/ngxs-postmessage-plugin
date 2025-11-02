674265e193550a29d9894de26cc0f7fea46b1d73
Nelson R. Graña <nelsongraa8@gmail.com>
2025-11-02 17:05:04 +0100

    feat(app): create a component children

---
+++ apps/demo/src/context/children/children.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-children',
+  template: `<p>children works!</p>`,
+  standalone: true,
+})
+export class ChildrenComponent {}
