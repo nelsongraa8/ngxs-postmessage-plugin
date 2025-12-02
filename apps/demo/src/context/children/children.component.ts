1326e92d5235b4546e48584ca552d3b177997268
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-03 00:02:56 +0100

    feat(app): eliminate app demo

--- apps/demo/src/context/children/children.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-children',
-  template: `<p>children works!</p>`,
-  standalone: true,
-})
-export class ChildrenComponent {}
