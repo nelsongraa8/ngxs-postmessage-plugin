b94f5d210d7a8e0d455b46a6599e8f9770193ab3
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-06 16:40:09 +0100

    feat(options): create interface to options of plugins

--- libs/postmessage-plugin/src/lib/symbols/interfaces/options.ts
+++ libs/postmessage-plugin/src/lib/symbols/interfaces/options.ts
@@ -1,23 +1,43 @@
 export interface NgxsPostmessagePluginOptions {
-  targets?: { name: string; origin: string[]; transfer?: unknown[] }[];
   send: SendOptions;
+  receive: ReceiveOptions;
 }
 
 export interface SendOptions {
-  actions: SendOptionAction[];
-  states: SendOptionState[];
+  actions?: SendOptionAction[];
+  states?: SendOptionState[];
+}
+
+export interface ReceiveOptions {
+  fromOrigin: string;
+  targetState: any;
+  actionType?: new (payload: any) => any;
+}
+
+export interface ReceiveOptionState {
+  type: any;
+  from?: string;
+  targets?: () => HTMLIFrameElement | HTMLIFrameElement[] | null;
+  mode?: 'change' | 'all';
+}
+
+export interface ReceiveOptionAction {
+  type: any;
+  from?: string;
+  targets?: () => HTMLIFrameElement | HTMLIFrameElement[] | null;
+  mode?: 'change' | 'all';
 }
 
 export interface SendOptionState {
-  type: string;
-  to?: string[];
+  type: any;
+  to?: string;
   targets?: () => HTMLIFrameElement | HTMLIFrameElement[];
   mode?: 'change' | 'all';
 }
 
 export interface SendOptionAction {
-  type: string;
-  to?: string[];
+  type: any;
+  to?: string;
   targets?: () => HTMLIFrameElement | HTMLIFrameElement[] | null;
   mode?: 'change' | 'all';
 }
