4707f6cc53a0f8260b9136e624a0fcf11220d190
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-06 17:16:43 +0100

    feat(plugin): send info through the state

--- libs/postmessage-plugin/src/lib/handlers/send-father.handler.ts
+++ libs/postmessage-plugin/src/lib/handlers/send-father.handler.ts
@@ -1,24 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { NgxsPostmessagePluginOptions } from '../symbols/interfaces/options';
+import { Store } from '@ngxs/store';
 
 @Injectable({ providedIn: 'root' })
 export class HandlerManager {
-  execute(
-    state: any,
-    action: any,
-    result: any,
+  private store?: Store;
+
+  constructor(private injector: Injector) {}
+
+  receive(options: NgxsPostmessagePluginOptions): void {
+    window.addEventListener('message', (event) => {
+      if (!this.store) {
+        this.store = this.injector.get(Store);
+      }
+    });
+  }
+
+  send(
+    { state, action, result }: { state: any; action: any; result: any },
     options: NgxsPostmessagePluginOptions
   ): void {
-    if (typeof window !== 'undefined' && window.postMessage) {
-      const objectToPost = {
-        type: '[NGXS] Action Dispatched',
-        action: this.sanitizeForPostMessage(action),
-        state: this.sanitizeForPostMessage(result),
-      };
-      const targetOrigin = '*';
-
-      window.postMessage(objectToPost, targetOrigin);
+    if (!options.send.actions) {
+      return;
     }
+
+    options.send.actions.forEach((sendAction) => {
+      if (!(sendAction.type === action.constructor)) {
+        return;
+      }
+
+      window.parent.postMessage(
+        this.sanitizeForPostMessage(action),
+        sendAction.to ?? '*'
+      );
+    });
   }
 
   private sanitizeForPostMessage(obj: unknown): unknown {
