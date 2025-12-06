706983d7ac925db42b7ffea9ca77f4be249fc8dc
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-06 16:47:46 +0100

    feat(ngxs-plugin): send and receive from plugins

--- libs/postmessage-plugin/src/lib/plugin/postmessage.plugin.ts
+++ libs/postmessage-plugin/src/lib/plugin/postmessage.plugin.ts
@@ -3,6 +3,7 @@ import { NgxsNextPluginFn, NgxsPlugin } from '@ngxs/store/plugins';
 import { NgxsPostmessagePluginOptions } from '../symbols/interfaces/options';
 import { NGXS_POSTMESSAGE_PLUGIN_OPTIONS } from '../symbols/tokens';
 import { HandlerManager } from '../handlers/send-father.handler';
+import { ReceiveHandler } from '../handlers/receive.handler';
 
 @Injectable({
   providedIn: 'root',
@@ -11,13 +12,16 @@ export class PostMessagePlugin implements NgxsPlugin {
   constructor(
     @Inject(NGXS_POSTMESSAGE_PLUGIN_OPTIONS)
     private options: NgxsPostmessagePluginOptions,
+    private receiveHandler: ReceiveHandler,
     private handlerManager: HandlerManager
-  ) {}
+  ) {
+    this.receiveHandler.receive(this.options);
+  }
 
   handle(state: any, action: any, next: NgxsNextPluginFn): void {
     const result = next(state, action);
 
-    this.handlerManager.execute(state, action, result, this.options);
+    this.handlerManager.send({ state, action, result }, this.options);
 
     return result;
   }
