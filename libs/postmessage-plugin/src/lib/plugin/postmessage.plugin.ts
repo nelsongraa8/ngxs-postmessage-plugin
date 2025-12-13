66b51cbe0c8b6455592e7612766a64420e32a54f
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-13 21:32:10 +0100

    feat(plugin): cancel calls to handlers in the plugin declaration

--- libs/postmessage-plugin/src/lib/plugin/postmessage.plugin.ts
+++ libs/postmessage-plugin/src/lib/plugin/postmessage.plugin.ts
@@ -2,8 +2,6 @@ import { Inject, Injectable } from '@angular/core';
 import { NgxsNextPluginFn, NgxsPlugin } from '@ngxs/store/plugins';
 import { NgxsPostmessagePluginOptions } from '../symbols/interfaces/options';
 import { NGXS_POSTMESSAGE_PLUGIN_OPTIONS } from '../symbols/tokens';
-import { HandlerManager } from '../handlers/send-father.handler';
-import { ReceiveHandler } from '../handlers/receive.handler';
 
 @Injectable({
   providedIn: 'root',
@@ -11,17 +9,11 @@ import { ReceiveHandler } from '../handlers/receive.handler';
 export class PostMessagePlugin implements NgxsPlugin {
   constructor(
     @Inject(NGXS_POSTMESSAGE_PLUGIN_OPTIONS)
-    private options: NgxsPostmessagePluginOptions,
-    private receiveHandler: ReceiveHandler,
-    private handlerManager: HandlerManager
-  ) {
-    this.receiveHandler.receive(this.options);
-  }
+    private options: NgxsPostmessagePluginOptions
+  ) {}
 
   handle(state: any, action: any, next: NgxsNextPluginFn): void {
     const result = next(state, action);
-
-    this.handlerManager.send({ state, action, result }, this.options);
 
     return result;
   }
