5c47cccf832a31c5e92e95ebfdfce02c681c9e88
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-13 21:54:06 +0100

    feat(index): reorganize exports and add types for the postmessage plugin

--- libs/postmessage-plugin/src/index.ts
+++ libs/postmessage-plugin/src/index.ts
@@ -1,4 +1,13 @@
-export * from './lib/symbols/interfaces/options';
-export * from './lib/symbols/tokens';
-export * from './lib/plugin/postmessage.plugin';
-export * from './lib/module/postmessage.module';
+// Provider and Module
+export { NgxsPostMessagePluginModule as NgxsPostMessagePluginModule } from './lib/module/postmessage.module';
+export { withNgxsPostMessagePlugin as withNgxsPostMessagePlugin } from './lib/module/ngxs-post-message-plugin.provider';
+
+// Interface for Options
+export type { NgxsPostmessagePluginOptions as NgxsPostmessagePluginOptions } from './lib/symbols/interfaces/options';
+
+// Actions and State for Embedded Post Message Bus
+export {
+  NgxsPmEmbeddedSend as NgxsPmEmbeddedSend,
+  NgxsPmEmbeddedReceive as NgxsPmEmbeddedReceive,
+} from './lib/handlers/embedded/embedded-post-message-bus.state';
+export { EmbeddedPostMessageBusState as EmbeddedPostMessageBusStateExport } from './lib/handlers/embedded/embedded-post-message-bus.state';
