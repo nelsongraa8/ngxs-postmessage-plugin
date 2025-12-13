f728b4946c56a2bb2d6d3d2cd264de4adc0742e5
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-13 21:58:56 +0100

    feat(config): simplify postmessage plugin configuration by removing unused options

--- apps/example-embedded/src/app/app.config.ts
+++ apps/example-embedded/src/app/app.config.ts
@@ -8,45 +8,7 @@ import { appRoutes } from './app.routes';
 import { provideStore } from '@ngxs/store';
 import { CounterState } from '../context/ngxs/counter.state';
 import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
-import {
-  NgxsPostmessagePluginOptions,
-  withNgxsPostMessagePlugin,
-} from '@ngxs-postmessage/postmessage-plugin';
-import { Decrement, Increment } from '../context/ngxs/counter.actions';
-
-export const SimplePostMessageConfig: NgxsPostmessagePluginOptions = {
-  send: {
-    actions: [
-      {
-        type: Increment,
-      },
-      {
-        type: Decrement,
-        to: 'https://analytics.app',
-      },
-    ],
-  },
-  receive: {
-    fromOrigin: 'http://localhost:4200',
-    targetState: CounterState,
-    // actionType: SetExternalData,
-  },
-};
-
-// export const ExtremePostMessageConfig: NgxsPostmessagePluginOptions = {
-//   targets: [
-//     { name: 'all', origin: ['*'], transfer: ['*'] },
-//     {
-//       name: 'analytics',
-//       origin: ['https://analytics.app'],
-//       transfer: [''],
-//     },
-//   ],
-//   send: {
-//     actions: [{ type: '*', targets: ['all'], mode: 'all' }],
-//     states: [{ type: '*', targets: ['all', 'analytics'], mode: 'change' }],
-//   },
-// };
+import { withNgxsPostMessagePlugin } from '@ngxs-postmessage/postmessage-plugin';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -60,7 +22,7 @@ export const appConfig: ApplicationConfig = {
         collapsed: false,
         logger: console,
       }),
-      withNgxsPostMessagePlugin(SimplePostMessageConfig)
+      withNgxsPostMessagePlugin()
     ),
   ],
 };
